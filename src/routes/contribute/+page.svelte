3573132c1c71ab8acf212c22c12ad982d5ecffdb
mkpoli <git@mkpo.li>
2026-01-16 15:03:12 +0900

    docs: add notice before contributing

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-import { page } from "$app/state";
-import * as m from "$lib/paraglide/messages";
+  import { page } from '$app/state';
+  import * as m from '$lib/paraglide/messages';
 
-let { data } = $props();
-let venue = $derived(data.venue);
+  let { data } = $props();
+  let venue = $derived(data.venue);
 </script>
 
 <div class="container mx-auto p-8 max-w-2xl">
@@ -93,6 +93,25 @@ let venue = $derived(data.venue);
           <option value="engineering">{m.area_engineering()}</option>
           <option value="science">{m.area_science()}</option>
         </select>
+      </div>
+    </div>
+
+    <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+      <div class="flex">
+        <div class="flex-shrink-0">
+          <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path
+              fill-rule="evenodd"
+              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div class="ml-3">
+          <p class="text-sm text-yellow-700">
+            {m.notice_public_domain()}
+          </p>
+        </div>
       </div>
     </div>
 
